b64d0538b95fdf085e46547ce05c280ed4f87ced
Rudra Pratap Singh <130541940+rudrapratap63@users.noreply.github.com>
2025-06-15 18:30:57 +0530

    setup mongoDb connection and basic express middleware

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js"
 
 dotenv.config({
     path : "./.env"
 })
-connectDB();
+connectDB()
+.then(() => {
+    const port = process.env.PORT || 8000;
+    app.listen(port, ()=>{
+        console.log(`Server is running at port ${port}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
