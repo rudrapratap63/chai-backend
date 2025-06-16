f781840fb0431ed492a8f2111dd6cc450cfc271c
Rudra Pratap Singh <130541940+rudrapratap63@users.noreply.github.com>
2025-06-16 10:16:45 +0530

    add user controller and route

--- src/app.js
+++ src/app.js
@@ -13,4 +13,8 @@ app.use(express.urlencoded({extended : true, limit : "16kb" }))
 app.use(cookieParser());
 app.use(express.static("public"));
 
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
 export { app };
